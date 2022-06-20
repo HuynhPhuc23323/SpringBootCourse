566f179a72794a89d5855170b44b2594048e028b
31191025291 <phuchuynh.31191025291@st.ueh.edu.vn>
2022-06-20 19:04:26 +0700

    Book CRUD by VSCODE, video 16,17/38

---
+++ SpringBoot-Video-16,17th/dto/BookRequest.java
@@ -0,0 +1,5 @@
+package milliwatt.phuc.crudbook.dto;
+
+public record BookRequest(String title, String author, int year) {
+    
+}
---
+++ SpringBoot-Video-16,17th/model/Book.java
@@ -0,0 +1,59 @@
+package milliwatt.phuc.crudbook.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    
+    private String id;
+    private String title;
+    private int year;
+    private String author;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }   
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+
+    public Book() {
+    }
+    
+    public Book(String id, String title, String author, int year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+}
